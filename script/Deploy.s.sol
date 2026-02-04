e5800d0b5413d8374e67aed540b5804629876562
redrum <99007375+R3DRVM@users.noreply.github.com>
2026-02-04 11:42:53 -0800

    fix(ci): add console2 import to Deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.24;
 
 import {Script} from "forge-std/Script.sol";
+import {console2} from "forge-std/console2.sol";
 
 import {MockUSDC} from "../src/MockUSDC.sol";
 import {ACLPool} from "../src/ACLPool.sol";
